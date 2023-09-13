1d6ec4dc086a0d9c0494c65dbcc29ddd79c9ec67
Ming Huan Yang <huan@huanmarv.cc>
2023-09-13 20:31:37 +0800

    030

--- find_min_distance.py
+++ find_min_distance.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     index_cols2 = ["站名", "經度", "緯度"]
 
     df1 = pd.read_csv("./data/station.csv", usecols=index_cols1)  # 光電站資料 0-43
-    df2 = pd.read_csv("./data/weather_station.csv", usecols=index_cols2)  # 觀測站資料 0-666
+    df2 = pd.read_csv("./data/weather_station.csv",
+                      usecols=index_cols2)  # 觀測站資料 0-666
     df3 = pd.read_csv('./data/dist.csv')
 
     dataframe = []
@@ -31,9 +32,11 @@ if __name__ == "__main__":
             if df3[solar[i]][k] < min:
                 min = df3[solar[i]][k]
                 index = k
-        temp=[solar_Lng[i],solar_Lat[i],weather[index],weather_Lng[index],weather_Lat[index]]
+        temp = [solar_Lng[i], solar_Lat[i], weather[index],
+                weather_Lng[index], weather_Lat[index]]
         dataframe.append(temp)
 
-    df = pd.DataFrame(dataframe, index=new_index, columns=["發電站經度","發電站緯度","觀測站","觀測站經度","觀測站緯度"])
+    df = pd.DataFrame(dataframe, index=new_index, columns=[
+                      "gen_station", "gen_lng", "gen_lat", "obv_station", "obv_lng", "obv_lat"])
 
-    df.round(2).to_csv("./data/mini_dist2.csv")
+    df.round(2).to_csv("./data/mini_dist.csv")
--- weather.py
+++ weather.py
@@ -11,11 +11,13 @@ def transformat(string: str) -> str:
     string = "%25".join(string.split("%"))
     return string
 
+
 def get_station_info(name: str) -> list:
     df = pd.read_csv("./data/weather_station.csv")
     fliter = (df["站名"] == name)
     info = df[fliter].iloc[0]
     return [info['站號'], info['海拔高度(m)']]
+
 
 def seperate(df: pd.DataFrame, year: int, month: int) -> pd.DataFrame:
     col = df.columns.to_list()
@@ -23,7 +25,7 @@ def seperate(df: pd.DataFrame, year: int, month: int) -> pd.DataFrame:
 
     for i in col:
         new_col.append(i[2])
-    
+
     new_col[0] = "date"
     temp_list = []
 
@@ -36,8 +38,6 @@ def seperate(df: pd.DataFrame, year: int, month: int) -> pd.DataFrame:
     print(new_df.head())
     return new_df
 
-        
-
 
 def get_month_data(name: str, year: int, month: int) -> pd.DataFrame:
     if month < 10:
@@ -45,9 +45,9 @@ def get_month_data(name: str, year: int, month: int) -> pd.DataFrame:
     else:
         month = str(month)
     info = get_station_info(name)
-    
+
     URL = "https://e-service.cwb.gov.tw/HistoryDataQuery/MonthDataController.do?command=viewMain&station={}&stname={}&datepicker={}-{}&altitude={}m#".format(
-        info[0], transformat(name), year, month,info[1]
+        info[0], transformat(name), year, month, info[1]
     )
     try:
         web = requests.get(URL)
@@ -60,9 +60,11 @@ def get_month_data(name: str, year: int, month: int) -> pd.DataFrame:
         return df
     except:
         raise ValueError("未找到站名 || 資訊取得錯誤")
-    
+
+
 def get_data(station: str) -> pd.DataFrame:
-    col = [('Unnamed: 0_level_0', '觀測時間 (day)', 'ObsTime'), ('press', '測站氣壓 (hPa)', 'StnPres'), ('press', '海平面氣壓 (hPa)', 'SeaPres'), ('press', '測站最高氣壓 (hPa)', 'StnPresMax'), ('press', '測站最高氣壓時間 (LST)', 'StnPresMaxTime'), ('press', '測站最低氣壓 (hPa)', 'StnPresMin'), ('press', '測站最低氣壓時間 (LST)', 'StnPresMinTime'), ('temperature', '氣溫 (℃)', 'Temperature'), ('temperature', '最高氣溫 (℃)', 'T Max'), ('temperature', '最高氣溫時間 (LST)', 'T Max Time'), ('temperature', '最低氣溫 (℃)', 'T Min'), ('temperature', '最低氣溫時間 (LST)', 'T Min Time'), ('Dew Point', '露點溫度 (℃)', 'Td dew point'), ('RH', '相對溼度 (%)', 'RH'), ('RH', '最小相對溼度 (%)', 'RHMin'), ('RH', '最小相對溼度時間 (LST)', 'RHMinTime'), ('WS/WD', '風速 (m/s)', 'WS'), ('WS/WD', '風向 (360degree)', 'WD'), ('WS/WD', '最大瞬間風 (m/s)', 'WSGust'), ('WS/WD', '最大瞬間風風向 (360degree)', 'WDGust'), ('WS/WD', '最大瞬間風風速時間 (LST)', 'WGustTime'), ('Precp', '降水量 (mm)', 'Precp'), ('Precp', '降水時數 (hour)', 'PrecpHour'), ('Precp', '最大十分鐘降水量 (mm)', 'PrecpMax10'), ('Precp', '最大十分鐘降水量起始時間 (LST)', 'PrecpMax10Time'), ('Precp', '最大六十分鐘降水量 (mm)', 'PrecpMax60'), ('Precp', '最大六十分鐘降水量起始時間 (LST)', 'PrecpMax60Time'), ('SunShine', '日照時數 (hour)', 'SunShine'), ('SunShine', '日照率 (%)', 'SunShineRate'), ('SunShine', '全天空日射量 (MJ/㎡)', 'GloblRad'), ('visibility', '能見度 (km)', 'VisbMean'), ('Evaperation', 'A型蒸發量 (mm)', 'EvapA'), ('UVI', '日最高紫外線指數', 'UVI Max'), ('UVI', '日最高紫外線指數時間 (LST)', 'UVI Max Time'), ('Cloud', '總雲量 (0~10)', 'Cloud Amount')]
+    col = [('Unnamed: 0_level_0', '觀測時間 (day)', 'ObsTime'), ('press', '測站氣壓 (hPa)', 'StnPres'), ('press', '海平面氣壓 (hPa)', 'SeaPres'), ('press', '測站最高氣壓 (hPa)', 'StnPresMax'), ('press', '測站最高氣壓時間 (LST)', 'StnPresMaxTime'), ('press', '測站最低氣壓 (hPa)', 'StnPresMin'), ('press', '測站最低氣壓時間 (LST)', 'StnPresMinTime'), ('temperature', '氣溫 (℃)', 'Temperature'), ('temperature', '最高氣溫 (℃)', 'T Max'), ('temperature', '最高氣溫時間 (LST)', 'T Max Time'), ('temperature', '最低氣溫 (℃)', 'T Min'), ('temperature', '最低氣溫時間 (LST)', 'T Min Time'), ('Dew Point', '露點溫度 (℃)', 'Td dew point'), ('RH', '相對溼度 (%)', 'RH'), ('RH', '最小相對溼度 (%)', 'RHMin'), ('RH', '最小相對溼度時間 (LST)', 'RHMinTime'), ('WS/WD', '風速 (m/s)', 'WS'), ('WS/WD', '風向 (360degree)',
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  'WD'), ('WS/WD', '最大瞬間風 (m/s)', 'WSGust'), ('WS/WD', '最大瞬間風風向 (360degree)', 'WDGust'), ('WS/WD', '最大瞬間風風速時間 (LST)', 'WGustTime'), ('Precp', '降水量 (mm)', 'Precp'), ('Precp', '降水時數 (hour)', 'PrecpHour'), ('Precp', '最大十分鐘降水量 (mm)', 'PrecpMax10'), ('Precp', '最大十分鐘降水量起始時間 (LST)', 'PrecpMax10Time'), ('Precp', '最大六十分鐘降水量 (mm)', 'PrecpMax60'), ('Precp', '最大六十分鐘降水量起始時間 (LST)', 'PrecpMax60Time'), ('SunShine', '日照時數 (hour)', 'SunShine'), ('SunShine', '日照率 (%)', 'SunShineRate'), ('SunShine', '全天空日射量 (MJ/㎡)', 'GloblRad'), ('visibility', '能見度 (km)', 'VisbMean'), ('Evaperation', 'A型蒸發量 (mm)', 'EvapA'), ('UVI', '日最高紫外線指數', 'UVI Max'), ('UVI', '日最高紫外線指數時間 (LST)', 'UVI Max Time'), ('Cloud', '總雲量 (0~10)', 'Cloud Amount')]
     new_col = ['date']
     for i in col:
         new_col.append(i[2])

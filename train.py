c2d1050f1d98e70cd854d8a04387267f9f7d68c1
Ming Huan Yang <mhy1264+github@gmail.com>
2023-10-17 07:59:16 +0800

    learning v2

--- train.py
+++ train.py
@@ -7,20 +7,20 @@ import autokeras as ak
 if __name__ == "__main__":
     train, test = split_data.split_data(100)
 
-    print(train.columns)
     x_columns = ['date', 'ObsTime', 'Temperature',
-                 'RH', 'SunShine', 'SunShineRate', 'UVI Max_x',
+                 'RH', 'SunShine', 'SunShineRate', 'UVI Max_y',
                  'Lng', 'Lat']
     y_columns = ['degree']
 
     x_train = train[x_columns]
+    x_train.dropna()
     y_train = train[y_columns]
 
     x_test = test[x_columns]
     y_test = test[y_columns]
 
     # It tries 10 different models.
-    reg = ak.StructuredDataRegressor(max_trials=3, overwrite=True)
+    reg = ak.StructuredDataRegressor(max_trials=10, overwrite=True)
     # Feed the structured data regressor with training data.
     reg.fit(x_train, y_train, epochs=100)
     # Predict with the best model.

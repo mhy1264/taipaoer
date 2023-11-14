dc9da93d16b00b886edcf18fdd225b8ef64b1f55
Ming Huan Yang <huan@huanmarv.cc>
2023-11-14 11:28:01 +0800

    normailze"

--- paritial_train.py
+++ paritial_train.py
@@ -11,6 +11,10 @@ def myprint(s):
         print(s, file=f)
 
 
+def mean_norm(df_input):
+    return df_input.apply(lambda x: (x - x.mean()) / x.std(), axis=0)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("filePath", help="file path")
@@ -20,19 +24,17 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    train, test = train_test_split(pd.read_csv(args.filePath), test_size=0.2)
+    train, test = train_test_split(pd.read_csv(args.filePath), test_size=0.3)
 
-    x_columns = ['date', 'ObsTime', 'Temperature',
-                 'RH', 'SunShine', 'SunShineRate', 'UVI Max',
-                 'Lng', 'Lat']
+    x_columns = ['ObsTime', 'Temperature',
+                 'RH', 'SunShine', 'UVI Max',]
     y_columns = ['degree']
 
-    x_train = train[x_columns]
+    x_train = mean_norm(train[x_columns])
     x_train.dropna()
-    x_train.to_csv("./xtrain.csv")
     y_train = train[y_columns]
 
-    x_test = test[x_columns]
+    x_test = mean_norm(test[x_columns])
     y_test = test[y_columns]
 
     # It tries 10 different models.
@@ -43,6 +45,3 @@ if __name__ == "__main__":
     predicted_y = reg.predict(x_test)
     # Evaluate the best model with testing data.
     print(reg.evaluate(x_test, y_test))
-    model = reg.export_model()
-    model.summary(print_fn=myprint)
-    model.summary()

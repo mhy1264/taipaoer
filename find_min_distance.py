cbc6fc725f04f269ae07a1276977cab6ff215d5a
Ming Huan Yang <huan@huanmarv.cc>
2023-11-25 17:31:00 +0800

    update column name

--- find_min_distance.py
+++ find_min_distance.py
@@ -27,19 +27,20 @@ if __name__ == "__main__":
         index = [0, 0]
         for k in range(len(weather)):
             if (df3[solar[i]][k] < min[0]) | (df3[solar[i]][k] < min[1]):
-                if min[0] >= min [1]:
+                if min[0] >= min[1]:
                     min[0] = df3[solar[i]][k]
                     index[0] = k
                 else:
                     min[1] = df3[solar[i]][k]
-                    index[1] = k                
-        temp = [solar[i], solar_Lng[i], solar_Lat[i], 
-                weather[index[0]], weather_Lng[index[0]], weather_Lat[index[0]],
+                    index[1] = k
+        temp = [solar[i], solar_Lng[i], solar_Lat[i],
+                weather[index[0]], weather_Lng[index[0]
+                                               ], weather_Lat[index[0]],
                 weather[index[1]], weather_Lng[index[1]], weather_Lat[index[1]]]
         dataframe.append(temp)
 
     df = pd.DataFrame(dataframe, columns=[
-                      "gen_station", "gen_lng", "gen_lat", "obv_station1", "obv_lng1", "obv_lat1", 
-                      "obv_station2", "obv_lng2", "obv_lat2"])
+                      "gen_station", "gen_lng", "gen_lat", "obv_station1", "obv1_lng", "obv1_lat",
+                      "obv_station2", "obv2_lng", "obv2_lat"])
 
     df.round(2).to_csv("./data/gen_obv_min_dist.csv")

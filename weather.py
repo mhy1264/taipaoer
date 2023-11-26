2b41f316b08acad04fe09a2e6b53065f76102652
Ming Huan Yang <huan@huanmarv.cc>
2023-11-26 15:13:19 +0800

    更改取得資料條件“

--- weather.py
+++ weather.py
@@ -9,12 +9,6 @@ from calendar import isleap
 import uva
 
 
-def transformat(string: str) -> str:
-    string = urllib.parse.quote(string)
-    string = "%25".join(string.split("%"))
-    return string
-
-
 def get_station_info(name: str) -> list:
     df = pd.read_csv("./data/weather_station.csv")
     fliter = (df["StationName"] == name)
@@ -42,28 +36,39 @@ def get_month_data(station: list, year: int, month: int, UVStation: str):
     res = requests.post(
         "https://codis.cwa.gov.tw/api/station?", data=data)
 
-    daily_data = json.loads(res.text)['data'][0]['dts']
+    with open("{}.csv".format(station[1]), 'w', encoding=res.encoding) as f:
+        f.write(res.text)
 
-    Datas = []
-    print(year, month)
-    for i in daily_data:
-        temp = [np.nan]*5
-        temp[0] = i['DataDate'][0:10]
-        temp[1] = i['AirTemperature']['Mean']
-        temp[2] = i['UVIndex']['Maximum'] if (
-            'UVIndex' in i and 'Maximin' in i['UVIndex']) else -1
-        temp[3] = i['SunshineDuration']['Total']
-        temp[4] = i['GlobalSolarRadiation']['Accumulation']
-        Datas.append(temp)
-    Datas.sort()
+    jsonObj = json.loads(res.text)
+    if jsonObj['metadata']['count'] != 0:
+        daily_data = json.loads(res.text)['data'][0]['dts']
 
-    df = pd.DataFrame(
-        Datas, columns=['Date', 'Temp', 'UV', 'SunShineHour', 'GlobalRad'])
-    if (df['UV'] == -1).all():
-        UVinfo = uva.get_data_by_month(int(year), int(month), UVStation)[
-            "UVI Max"].to_list()
-        df['UV'] = UVinfo
-    return df
+        Datas = []
+        print(year, month)
+        for i in daily_data:
+            temp = [np.nan]*6
+            temp[0] = i['DataDate'][0:10]
+            temp[1] = i['AirTemperature']['Mean']
+            temp[2] = i['UVIndex']['Maximum'] if (
+                'UVIndex' in i and 'Maximum' in i['UVIndex']) else -1
+            temp[3] = i['SunshineDuration']['Total'] if (
+                "SunshineDuration" in i and "Total" in i['SunshineDuration']) else -1
+            temp[4] = i['GlobalSolarRadiation']['Accumulation'] if (
+                'GlobalSolarRadiation' in i and 'Accumulation' in i['GlobalSolarRadiation']) else -1
+            temp[5] = False
+            Datas.append(temp)
+        Datas.sort()
+
+        df = pd.DataFrame(
+            Datas, columns=['date', 'Temp', 'UV', 'SunShineHour', 'GlobalRad', "HaveEmpty"])
+        # if (df['UV'] == -1).all():
+        #     UVinfo = uva.get_data_by_month(int(year), int(month), UVStation)[
+        #         "UVI Max"].to_list()
+        #     df['UV'] = UVinfo
+        #     df['HaveEmpty'] = [True]*len(UVinfo)
+        return df
+    else:
+        raise ValueError("mata Count = 0")
 
 
 def get_data(station: str, UVStation) -> pd.DataFrame:

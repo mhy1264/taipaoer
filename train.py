7b5ab4be94fee6315d897f769879b3cb80052e78
Ming Huan Yang <huan@huanmarv.cc>
2023-11-26 22:00:22 +0800

    改用 kfold 框架訓練

--- train.py
+++ train.py
@@ -5,33 +5,51 @@ import tensorflow as tf
 import autokeras as ak
 import argparse
 
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import autokeras as ak
+import argparse
+
+import numpy as np
+from sklearn.model_selection import KFold
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("epochs", help="epoch 數", type=int)
-    parser.add_argument("maxtrial",
-                        help="maxtrial", type=int)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("epochs", help="epoch 數", type=int)
+    # parser.add_argument("maxtrial",
+    #                     help="maxtrial", type=int)
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
-    train, test = split_data.split_data_by_station('蘭嶼電廠光電', 100)
-
-    x_columns = ['Temperature',
-                 'RH', 'SunShineRate', 'UVI Max',
-                 'Lng', 'Lat']
+    x_columns = ['Temp', 'UV', 'SunShineHour', 'GlobalRad']
     y_columns = ['degree']
 
-    x_train = train[x_columns]
-    x_train.dropna()
-    y_train = train[y_columns]
+    data = split_data.k_fold(100)
+    data.to_csv("./total.csv")
+    print(data.shape)
 
-    x_test = test[x_columns]
-    y_test = test[y_columns]
+    reg = ak.StructuredDataRegressor(max_trials=100, overwrite=True)
 
-    # It tries 10 different models.
-    reg = ak.StructuredDataRegressor(max_trials=args.maxtrial, overwrite=True)
-    # Feed the structured data regressor with training data.
-    reg.fit(x_train, y_train, epochs=args.epochs)
-    # Predict with the best model.
-    predicted_y = reg.predict(x_test)
-    # Evaluate the best model with testing data.
-    print(reg.evaluate(x_test, y_test))
+    kf = KFold(n_splits=10)
+    kf.get_n_splits(data)
+
+    for i, (train_index, test_index) in enumerate(kf.split(data)):
+
+        print(f"Fold {i}:")
+        x_train = data.iloc[train_index][x_columns]
+        y_train = data.iloc[train_index][y_columns]
+
+        x_test = data.iloc[test_index][x_columns]
+        y_test = data.iloc[test_index][y_columns]
+
+        # Feed the structured data regressor with training data.
+        reg.fit(x_train, y_train, epochs=100)
+        # Predict with the best model.
+        predicted_y = reg.predict(x_test)
+        # Evaluate the best model with testing data.
+        print(reg.evaluate(x_test, y_test))
+
+    model = reg.export_model()
+    model.summary()

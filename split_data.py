28233636b7caa2ff4dbf965d996857b6864ba8ea
Ming Huan Yang <huan@huanmarv.cc>
2023-11-26 15:12:21 +0800

    增加 kfold

--- split_data.py
+++ split_data.py
@@ -1,14 +1,10 @@
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 import pandas as pd
 import numpy as np
 import os
 import math
 folder_path = './weather_data'
 solar_station = os.listdir(folder_path)
-
-# print (solar_station)
-
-# train = pd.DataFrame(columns=data.columns)
 
 
 def split_data_by_station(station: str, percentage=100):
@@ -53,8 +49,27 @@ def split_data(percentage=100):
     return train, test
 
 
+def k_fold(data: pd.DataFrame):
+    x_columns = ['Temp', 'UV', 'SunShineHour', 'GlobalRad']
+    y_columns = ['degree']
+
+    X = data[x_columns]
+    y = data[y_columns]
+    kf = KFold(n_splits=2)
+    kf.get_n_splits()
+    for X_train, X_test in kf.split(X):
+        print(X_train.tolist())
+        print("-----")
+        print(X_test.tolist())
+        print("======")
+
+
 if __name__ == "__main__":
-    train, test = split_data_by_station('蘭嶼電廠光電', 100)
-    print(train, test)
-    train.to_csv("./train.csv")
-    test.to_csv("./test.csv")
+    data = pd.read_csv("nwd/中科E\S光電_臺中_沙鹿.csv")
+    data = data[data['degree'] > 0]
+
+    k_fold(data)
+    # train, test = split_data_by_station('蘭嶼電廠光電', 100)
+    # print(train, test)
+    # train.to_csv("./train.csv")
+    # test.to_csv("./test.csv")

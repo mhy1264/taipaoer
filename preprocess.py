ddcc65ddc932c59fdbdfc0a5648054caaa2bdae2
Ming Huan Yang <mhy1264+github@gmail.com>
2023-12-11 19:56:09 +0800

    在訓練資料及新增平均單位裝置容量每日發電量

--- preprocess.py
+++ preprocess.py
@@ -29,19 +29,19 @@ if __name__ == "__main__":
 
     solar_daily = pd.read_csv("./data/solar_daily.csv")
     weather_station = pd.read_csv("./data/weather_station.csv")
+    form_data = pd.read_csv("data/station_unit_transform.csv")
 
     for row in range(1, solar_daily.shape[1]):
         df = solar_daily.iloc[:, [row, -1]]
         current_station = df.columns.tolist()[0]
-
-        obv_station, uva_station = get_obv_station(current_station)
-        if (obv_station == None and uva_station == None):
-            continue
+        fileName = current_station.replace("/", "\\")
+        # obv_station, uva_station = get_obv_station(current_station)
+        # if (obv_station == None and uva_station == None):
+        #     continue
 
         try:
-            station_weather = weather.get_data(obv_station, uva_station)
-
-            fileName = current_station.replace("/", "\\")
+            station_weather = weather.preprocess_data_weather(
+                current_station+"站")
 
             # # 合併發電站度數和天氣觀測站的天氣資訊
             station_weather['date'] = station_weather['date'].astype(str)
@@ -53,12 +53,14 @@ if __name__ == "__main__":
             # 合併 merge_df 和紫外線資料 放到 UVI max 這格內
 
             merged_df["station"] = [current_station]*merged_df.shape[0]
+            merged_df["capacity"] = [form_data[form_data['name'].astype(
+                str).str.contains("{}".format(current_station))]['capacity'].values[0]]*merged_df.shape[0]
             merged_df = merged_df[merged_df['degree'] > 0]
+            merged_df = merged_df.round(2)
 
-            print(merged_df.head())
             try:
                 merged_df.to_csv(
-                    "./new_weather_data/{}_{}_{}.csv".format(fileName, obv_station, uva_station))
+                    "./new2_weather_data/{}.csv".format(fileName))
             except Exception as e:
                 print("{} Error: {}".format(fileName, e.args))
         except Exception as e:
